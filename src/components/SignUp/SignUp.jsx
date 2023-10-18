e73565d9bfb77e902404d8f90fe0ef06bf08c127
Shashvat <shashvatpatel2803@gmail.com>
2023-10-19 01:47:22 +0530

    minor bugs fixed

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -70,20 +70,17 @@ export default function SignUp() {
       const res = await createUserWithEmailAndPassword(auth, email, password);
       const storageRef = ref(storage, displayName);
       const uploadTask = uploadBytesResumable(storageRef, file);
-      console.log(res);
-
-
       uploadTask.on('state_changed',
         (snapshot) => {
           // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
           const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log('Upload is ' + progress + '% done');
+          // console.log('Upload is ' + progress + '% done');
           switch (snapshot.state) {
             case 'paused':
-              console.log('Upload is paused');
+              // console.log('Upload is paused');
               break;
             case 'running':
-              console.log('Upload is running');
+              // console.log('Upload is running');
               break;
           }
         },
@@ -92,7 +89,7 @@ export default function SignUp() {
         },
         () => {
           getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-            console.log('File available at', downloadURL);
+            // console.log('File available at', downloadURL);
             await updateProfile(res.user, {
               displayName,
               photoURL: downloadURL,
